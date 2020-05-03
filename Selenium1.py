c12b01b955a8d0a4021938a77533475b0194fc64
SwagTiger28 <SwagTiger28@gmail.com>
2020-05-03 19:12:58 -0400

    commit on master

--- Selenium1.py
+++ Selenium1.py
@@ -1,9 +1,30 @@
 from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://youtu.be/dQw4w9WgXcQ?t=1")
+driver.get("https://techwithtim.net/")
 print(driver.title)
-driver.quit()
-# driver.close() # Closes current tab
-# driver.quit()  # Closes browser
+
+search = driver.find_element_by_name('s')
+search.send_keys("test") # test is the string we're searching for
+search.send_keys(Keys.RETURN) # return is enter key
+
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'main'))
+    )
+
+    articles = main.find_elements_by_tag_name("article")
+    for article in articles:
+        header = article.find_element_by_class_name("entry-summary")
+        print(header.text)
+finally:
+    driver.quit()
+
+driver.quit()  # Closes browser
